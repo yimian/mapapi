dd2d68b9b90ff3d98334e43c05da58a6c0dce3bc
zhangjinjie <zhangjinjie@yimian.com.cn>
2015-11-09 21:46:19 +0800

    update

--- baidu/__init__.py
+++ baidu/__init__.py
@@ -2,12 +2,19 @@
 """ use as main map interface. """
 
 from scheduler import RoundRobinScheduler
+from location_api import LocationApi
+from place_api import PlaceApi
+from transform_api import TransformApi
 
 
 class MapApi(object):
-    def __init__(self, aks=None, **kwargs):
+    def __init__(self, aks=None, scheduler=None, **kwargs):
         super(MapApi, self).__init__()
         self.aks = aks
+        self.scheduler = scheduler
+        self._place_api_inst = None
+        self._location_api_inst = None
+        self._transform_api_inst = None
 
     def set_aks(self, aks):
         """
@@ -17,14 +24,52 @@ class MapApi(object):
         """
         self.aks = aks
 
+    def set_scheduler(self, scheduler):
+        """
+        set scheduler.
+
+        :param scheduler:
+        """
+        self.scheduler = scheduler
+
+    @property
     def place_api(self):
         """
         get place api interface.
+
+        :return: PlaceApi instance.
         """
-        pass
+        if self._place_api_inst:
+            return self._place_api_inst
+        if not self.scheduler:
+            self.scheduler = RoundRobinScheduler(self.aks)
+        self._place_api_inst = PlaceApi(self.scheduler)
+        return self._place_api_inst
 
-    def geo_api(self):
-        pass
+    @property
+    def location_api(self):
+        """
+        get location api interface.
 
+        :return: LocationApi instance.
+        """
+        if self._location_api_inst:
+            return self._location_api_inst
+        if not self.scheduler:
+            self.scheduler = RoundRobinScheduler(self.aks)
+        self._location_api_inst = LocationApi(self.scheduler)
+        return self._location_api_inst
+
+    @property
     def transform_api(self):
-        pass
+        """
+        get transform api interface.
+
+        :return: TransformApi instance.
+        """
+        if self._transform_api_inst:
+            return self._transform_api_inst
+        if not self.scheduler:
+            self.scheduler = RoundRobinScheduler(self.aks)
+        self._transform_api_inst = TransformApi(self.scheduler)
+        return self._transform_api_inst
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,7 @@ from test_location_api import TestLocationApi
 from test_place_api import TestPlaceApi
 from test_scheduler import TestRoundRobinScheduler
 from test_transform_api import TestTransformApi
+from test_map_api import TestMapApi
 
 if __name__ == '__main__':
     unittest.main()
