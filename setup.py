d73faa3332cd29a8349148b50bbce6bb0a660a1b
zhangjinjie <zhangjinjie@yimian.com.cn>
2015-11-10 11:33:30 +0800

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='mapapi',
       version='0.1',
       description='map web api, current support baidu',
-      url='',
+      url='https://bitbucket.org/zhangjinjie/mapapi/',
       author='zhangjinjie',
       author_email='zhangjinjie@yimian.com.cn',
       license='MIT',
