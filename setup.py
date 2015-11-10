a10baf8efd6f9449990ea1f7ad52dacdf8fee846
zhangjinjie <zhangjinjie@yimian.com.cn>
2015-11-10 11:32:12 +0800

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mapapi',
+      version='0.1',
+      description='map web api, current support baidu',
+      url='',
+      author='zhangjinjie',
+      author_email='zhangjinjie@yimian.com.cn',
+      license='MIT',
+      packages=['mapapi'],
+      zip_safe=False)
